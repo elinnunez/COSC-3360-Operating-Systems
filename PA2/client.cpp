47e1b77c5f97ca5b52a3f939f5cd11e07898c932
Elinnoel Nunez <elinnoelnunez@outlook.com>
2022-03-20 17:24:44 -0500

    Added PA2

---
+++ PA2/client.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <vector>
+
+struct D
+{
+    std::string encoded;
+    char decoded;
+    int portno;
+    char* servername;
+
+};
+
+void *Solve(void *x_ptr)
+{
+    struct D *temp_ptr = (struct D *)x_ptr;
+
+    int newsockfd, portnumber;
+
+    struct sockaddr_in server_address;
+    struct hostent *server;
+
+    portnumber = temp_ptr->portno;
+
+    newsockfd = socket(AF_INET, SOCK_STREAM, 0); // 0 is default protocol
+
+    if (newsockfd < 0)
+    {
+        std::cout << "Error creating socket in thread\n";
+        exit(0);
+    }
+
+    server = gethostbyname(temp_ptr->servername); // replace with getaddrinfo()?
+
+    if (server == nullptr)
+    {
+        std::cout << "Error, no such host\n";
+        exit(0);
+    }
+
+    memset((char *)&server_address, 0, sizeof(server_address)); // memset(void *s, int c, size_t n): Fills te first n bytes of the memory area pointed to by s with the constant byte c
+
+    server_address.sin_family = AF_INET;
+
+    memcpy((char *)&server_address.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+
+    server_address.sin_port = htons(portnumber);
+
+    int connection_status = connect(newsockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    if (connection_status < 0)
+    {
+        std::cout << "Connection Error in thread\n";
+        exit(0);
+    }
+
+    int n;
+
+    char charr[256];
+
+    memset(charr, 0, 256);
+
+    strcpy(charr, temp_ptr->encoded.c_str());
+
+    // std::cout << "Writing: " << charr << std::endl;
+
+    n = write(newsockfd, &charr, sizeof(charr));
+
+    if (n < 0)
+    {
+        std::cout << "Error writing to socket from thread.\n";
+        exit(0);
+    }
+    
+    n = read(newsockfd, &temp_ptr->decoded, sizeof(char));
+
+    if (n < 0)
+    {
+        std::cout << "Error reading from socket from thread.\n";
+    }
+
+    // std::cout << "Server to Thread Response:\n";
+    // std::cout << "---------------------\n";
+    // std::cout << "Encoded Char: " << temp_ptr->encoded << "\n";
+    // std::cout << "Decoded Char: " << temp_ptr->decoded << "\n";
+    // std::cout << "---------------------\n";
+
+    close(newsockfd);
+
+    return nullptr;
+
+}
+
+int main(int argc, char *argv[]) // argc == argument count, argv is the array of the arguments input
+{
+
+    int sockfd, portnumber, n, numbits;
+
+    struct sockaddr_in server_address;
+    struct hostent *server;
+
+    char binmsg[256];
+
+    if (argc < 3)
+    {
+        std::cout << "usage " << argv[0] << " hostname port\n";
+        exit(0);
+    }
+
+    portnumber = atoi(argv[2]); // argument at arg[2] to int
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // 0 is default protocol
+
+    if (sockfd < 0)
+    {
+        std::cout << "Error creating socket\n";
+        exit(0);
+    }
+
+    server = gethostbyname(argv[1]); // replace with getaddrinfo()?
+
+    if (server == nullptr)
+    {
+        std::cout << "Error, no such host\n";
+        exit(0);
+    }
+
+    memset((char *)&server_address, 0, sizeof(server_address)); // memset(void *s, int c, size_t n): Fills te first n bytes of the memory area pointed to by s with the constant byte c
+
+    server_address.sin_family = AF_INET;
+
+    memcpy((char *)&server_address.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+
+    server_address.sin_port = htons(portnumber);
+
+    int connection_status = connect(sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    if (connection_status < 0)
+    {
+        std::cout << "Connection Error\n";
+        exit(0);
+    }
+
+    memset(binmsg, 0, 256);
+
+    // std::cout << "Enter Binary Message: \n";
+    std::string stringval;
+    getline(std::cin >> std::ws, stringval);
+    strcpy(binmsg, stringval.c_str());
+
+    n = write(sockfd, &binmsg, sizeof(binmsg));
+
+    if (n < 0)
+    {
+        std::cout << "Error writing to socket.\n";
+        exit(0);
+    }
+
+    n = read(sockfd, &numbits, sizeof(int));
+
+    if (n < 0)
+    {
+        std::cout << "Error reading from socket.\n";
+    }
+
+    // std::cout << "Server Response:\n";
+    // std::cout << "---------------------\n";
+    // std::cout << "Num of bits: " << numbits << "\n";
+    // std::cout << "---------------------\n";
+
+    close(sockfd);
+
+    int MTHREADS = stringval.size() / numbits;
+
+    pthread_t mth[MTHREADS];
+
+    std::vector<struct D> charArr(MTHREADS);
+
+    for (int i = 0; i < MTHREADS; i++)
+    {
+       
+        std::string newstr = stringval.substr(0, numbits);
+
+        stringval = stringval.substr(numbits);
+
+        struct D dobj;
+        dobj.encoded = newstr;
+        dobj.portno = portnumber;
+        dobj.servername = argv[1];
+
+        charArr[i] = dobj;
+
+        if (pthread_create(&mth[i], nullptr, Solve, &charArr[i]))
+        {
+            std::cout << "Error creating thread" << std::endl;
+            return 1;
+        }
+
+    }
+
+    for (int i = 0; i < MTHREADS; i++)
+    {
+        pthread_join(mth[i], nullptr);
+    }
+
+    std::string decompressed = "";
+
+    for(auto &it: charArr) {
+        decompressed+= it.decoded;
+    }
+    std::cout << "Decompressed message: " << decompressed << "\n";
+
+    return 0;
+}
---
+++ PA2/server.cpp
@@ -0,0 +1,232 @@
+#include <iostream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
+#include <cmath>
+#include <regex>
+#include <unordered_map>
+
+struct obj
+{
+    char cval;
+    int dec;
+    std::string bin;
+};
+
+std::string dectobin(int num, int maxsize)
+{
+    std::string longbin = std::bitset<64>(num).to_string();
+
+    std::string cur = "";
+
+    cur = longbin.substr(64 - maxsize);
+
+    return cur;
+}
+
+void readInput(std::vector<obj> &objL, int &MAX_SIZE)
+{
+    std::string count;
+    getline(std::cin, count);
+
+    int maxi = 0;
+
+    std::string line;
+
+    for (int i = 0; i < stoi(count); i++)
+    {
+
+        getline(std::cin, line);
+
+        char c = line[0];
+        int curnum = stoi(line.substr(2)); // gets the rest of the line into a string, so any length number. but it skips over the whitespace
+
+        obj object;
+
+        object.cval = c;
+        object.dec = curnum;
+
+        objL.push_back(object);
+
+        maxi = std::max(maxi, curnum);
+    }
+
+    MAX_SIZE = std::ceil(log2(maxi + 1));
+}
+
+void fireman(int)
+{
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+        // std::cout << "A child process ended" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+
+    int MAX_SIZE;
+    std::vector<obj> objL;
+    readInput(objL, MAX_SIZE);
+
+    std::unordered_map<std::string, char> hashmap;
+
+    for (auto &curobj : objL)
+    {
+        curobj.bin = dectobin(curobj.dec, MAX_SIZE);
+        hashmap[curobj.bin] = curobj.cval;
+        // std::cout << "Char: " << curobj.cval << " Dec: " << curobj.dec << " Bin: " << curobj.bin << "\n";
+    }
+
+    int sockfd, newsockfd, portnumber, cli_len;
+
+    char binmsg[256];
+
+    struct sockaddr_in server_addr, cli_addr;
+
+    int n;
+
+    if (argc < 2)
+    {
+        std::cout << "ERROR, no port provided\n";
+        exit(1);
+    }
+
+    // Create the socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0)
+    {
+        std::cout << "ERROR opening socket\n";
+        exit(1);
+    }
+
+
+    //binding issue fix from https://stackoverflow.com/questions/24194961/how-do-i-use-setsockoptso-reuseaddr
+    int reuse = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, sizeof(reuse)) < 0) {
+        perror("setsockopt(SO_REUSEADDR) failed");
+    }
+
+    // Set all bits of the field to 0
+    memset((char *)&server_addr, 0, sizeof(server_addr));
+
+    // Set argument to int for portnumber
+    portnumber = atoi(argv[1]);
+
+    // Configure settings of the server address struct
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(portnumber);
+
+    // Bind the address struct to the socket
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        // std::cout << "ERROR on binding\n";
+        perror("Binding Error");
+        exit(1);
+    }
+
+    // Listen on the socket with max_size+1 max connections requests queued
+    listen(sockfd, MAX_SIZE + 1);
+
+    cli_len = sizeof(cli_addr);
+
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&cli_len);
+
+    if (newsockfd < 0)
+    {
+        perror("ERROR on accept");
+        exit(1);
+    }
+
+    memset(binmsg, 0, 256);
+
+    n = read(newsockfd, binmsg, sizeof(binmsg));
+
+    if (n < 0)
+    {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+
+    // std::cout << "Binary message from client: " << binmsg << "\n";
+
+    n = write(newsockfd, &MAX_SIZE, sizeof(int));
+
+    if (n < 0)
+    {
+        perror("ERROR writing to socket");
+       
+        exit(1);
+    }
+
+    close(newsockfd);
+
+    int newsock;
+
+    signal(SIGCHLD, fireman);
+    while (true)
+    {
+        newsock = accept(sockfd, (struct sockaddr *)&cli_addr, (socklen_t *)&cli_len);
+
+        if (newsock < 0)
+        {
+            perror("New socket error on accept");
+            exit(1);
+        }
+
+        int pid = fork();
+
+        if(pid < 0) {
+            perror("Error on fork");
+            exit(1);
+        }
+
+        if (pid == 0)
+        {
+            // std::cout << "A child process started" << std::endl;
+            close(sockfd);
+
+            char buffer[256];
+
+            memset(buffer, 0, 256);
+
+            int num = read(newsock, buffer, sizeof(buffer));
+
+            if (num < 0)
+            {
+                perror("ERROR reading from socket in fork");
+                close(newsock);
+                exit(1);
+            }
+
+            // std::cout << "Here is the bin str: " << buffer << "\n";
+
+            n = write(newsock, &hashmap[buffer], sizeof(char));
+
+            if (n < 0)
+            {
+                perror("ERROR writing to socket from fork");
+                close(newsock);
+                exit(1);
+            }
+            _exit(0);
+        }
+        else
+        {
+            close(newsock);
+        }
+        std::cin.get();
+    }
+
+    close(newsock);
+
+    close(sockfd);
+
+    return 0;
+}
